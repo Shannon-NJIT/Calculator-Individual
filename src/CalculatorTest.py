c4dc671f6ea8cf60865f0f19205bdbd613e14e93
Shannon <sp358@njit.edu>
2019-10-27 16:43:49 -0400

    Fix: adjusted pprint to be outside the for loop for each calculator

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -16,42 +16,42 @@ class TestCalculator(unittest.TestCase):
         for row in addition_list:
             self.assertEqual(self.calculator.addition(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-            # pprint(addition_list)
+        # pprint(addition_list)
 
     def test_subtraction(self):
         subtraction_list = csvreader('src/UnitTestSubtraction.csv').data
         for row in subtraction_list:
             self.assertEqual(self.calculator.subtraction(row['Value 2'], row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-            # pprint(subtraction_list)
+        # pprint(subtraction_list)
 
     def test_multiplication(self):
         multiplication_list = csvreader('src/UnitTestMultiplication.csv').data
         for row in multiplication_list:
             self.assertEqual(self.calculator.multiplication(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-            # pprint(multiplication_list)
+        # pprint(multiplication_list)
 
     def test_square(self):
         square_list = csvreader('src/UnitTestSquare.csv').data
         for row in square_list:
             self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-            # pprint(square_list)
+        # pprint(square_list)
 
     def test_division(self):
         division_list = csvreader('src/UnitTestDivision.csv').data
         for row in division_list:
             self.assertAlmostEqual(self.calculator.division(row['Value 2'], row['Value 1']), float(row['Result']))
             self.assertAlmostEqual(self.calculator.result, float(row['Result']))
-            # pprint(division_list)
+        # pprint(division_list)
 
     def test_sqrt(self):
         sqrt_list = csvreader('src/UnitTestSquareRoot.csv').data
         for row in sqrt_list:
             self.assertAlmostEqual(self.calculator.sqrt(row['Value 1']), float(row['Result']))
             self.assertAlmostEqual(self.calculator.result, float(row['Result']))
-            pprint(sqrt_list)
+        pprint(sqrt_list)
 
 
 if __name__ == '__main__':
