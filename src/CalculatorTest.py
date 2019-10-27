1ca28ad1bb88a19dfe26dafc27b50e9463f2d447
Shannon <sp358@njit.edu>
2019-10-27 16:10:31 -0400

    Feature: wrote calc instantiation and setup. ran calculator instantiation test  (pass)

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,9 +1,15 @@
-import unittest
+import unittest, csv
+from Calculator import Calculator
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
 
 
 if __name__ == '__main__':
